e467e17e2d3e2d8f1b0ff91618406f9a58eeea7f
Cbutler1 <collin.p.butler1@gmail.com>
2022-07-06 16:51:09 -0400

    second commit, removed notes

--- BasicForLoopsLab/Program.cs
+++ BasicForLoopsLab/Program.cs
@@ -3,12 +3,6 @@ string reset;
 do
 {
     Console.WriteLine("Enter a number: ");
-    //int i = int.Parse(Console.ReadLine());
-    //for (int addedNumbers = i; addedNumbers > 0; addedNumbers++)
-    //{
-    //    Console.WriteLine(addedNumbers);
-    //}
-
     int sum = 0;
     int input = int.Parse(Console.ReadLine());
     for (int x = input; x > 0; x--)
@@ -20,25 +14,3 @@ do
     reset = Console.ReadLine().ToLower();
 } while (reset.Equals("y"));
 Console.WriteLine("Goodbye!");
-
-
-
-
-
-//int i = int.Parse(Console.ReadLine());
-//for (int j = i; j >= 0; j--)
-//{
-//    Console.WriteLine(j);
-//    if (j == 0)
-//    {
-//        break;
-//    }
-//}
-//for (int x = 0; x <= i; x++)
-//{
-//    Console.WriteLine(x);
-//    if (i == x)
-//    {
-//        break;
-//    }
-//}
