1ba08985872b248cdd5afa9c77c144ddd008f240
Cbutler1 <collin.p.butler1@gmail.com>
2022-07-06 16:48:19 -0400

    first commit for basic loops

---
+++ BasicForLoopsLab/Program.cs
@@ -0,0 +1,44 @@
+﻿
+string reset;
+do
+{
+    Console.WriteLine("Enter a number: ");
+    //int i = int.Parse(Console.ReadLine());
+    //for (int addedNumbers = i; addedNumbers > 0; addedNumbers++)
+    //{
+    //    Console.WriteLine(addedNumbers);
+    //}
+
+    int sum = 0;
+    int input = int.Parse(Console.ReadLine());
+    for (int x = input; x > 0; x--)
+    {
+        sum += x;
+    }
+    Console.WriteLine("Sum is {0}", sum);
+    Console.WriteLine("Would you like to restart? y/n");
+    reset = Console.ReadLine().ToLower();
+} while (reset.Equals("y"));
+Console.WriteLine("Goodbye!");
+
+
+
+
+
+//int i = int.Parse(Console.ReadLine());
+//for (int j = i; j >= 0; j--)
+//{
+//    Console.WriteLine(j);
+//    if (j == 0)
+//    {
+//        break;
+//    }
+//}
+//for (int x = 0; x <= i; x++)
+//{
+//    Console.WriteLine(x);
+//    if (i == x)
+//    {
+//        break;
+//    }
+//}
